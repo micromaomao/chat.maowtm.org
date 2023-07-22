5d3c7795d6361232069596cfb6e106043dad640f
Mao <m@maowtm.org>
2023-07-22 18:10:33 +0100

    SQL schema

--- app/src/db/drop_everything.sql
+++ app/src/db/drop_everything.sql
@@ -1,4 +1,15 @@
-drop table if exists db_migration_state;
-drop table if exists global_configuration;
+drop table if exists db_migration_state cascade;
+drop table if exists global_configuration cascade;
+drop table if exists captcha_challenge cascade;
+drop table if exists chat_session cascade;
+drop table if exists chat_message cascade;
+drop table if exists chat_message_embedding cascade;
+drop table if exists stored_dialogue cascade;
+drop table if exists dialogue_item cascade;
+drop table if exists dialogue_phrasing cascade;
+drop table if exists dialogue_phrasing_embedding cascade;
+drop table if exists chat_reply_edit_log cascade;
+drop table if exists chat_reply_metadata cascade;
+drop table if exists chat_suggestion cascade;
 
 drop extension if exists ulid;
--- app/src/db/schema.sql
+++ app/src/db/schema.sql
@@ -2,7 +2,8 @@ create extension if not exists ulid;
 
 -- Used to track database version, to aid automatic migrations in the future.
 create table db_migration_state (
-  db_compat_version integer not null -- incremented every time an incompatible change is made to the schema
+  -- Incremented every time an incompatible change is made to the schema
+  db_compat_version integer not null
 );
 insert into db_migration_state (db_compat_version) values (1);
 
@@ -11,4 +12,164 @@ create table global_configuration (
   id ulid not null default gen_ulid() primary key,
   config json not null,
   app_version text not null
+);
+
+create table captcha_challenge (
+  challenge_token bytea not null primary key,
+  response_id text not null,
+  source_ip inet not null,
+  created_time timestamptz not null default now(),
+  solve_time timestamptz default null,
+  -- References chat_session
+  associated_session ulid default null
+);
+
+create table chat_session (
+  -- Not secret identifier for this chat session
+  session_id ulid not null default gen_ulid() primary key,
+
+  -- Secret used for fetching / sending messages
+  session_token bytea not null unique,
+
+  -- Required to refresh if too old, depending on server settings
+  last_captcha bytea default null references captcha_challenge (challenge_token) on delete set null,
+
+  -- References to stored_dialogue. Cache for determining "ignore_for_first_match"
+  last_matched_dialogues ulid[] not null default '{}'
+);
+
+-- alter table captcha_challenge add foreign key (associated_session) references chat_session (session_id) on delete set null;
+-- The above gives ERROR:  XX000: could not find commutator for operator 16397
+
+create table chat_message (
+  id ulid not null default gen_ulid() primary key,
+  session ulid not null references chat_session (session_id) on delete cascade,
+
+  -- enum: 0 = bot, 1 = user
+  msg_type int not null,
+
+  content text not null,
+
+  -- true if this message has been replaced by an attempted "regenerate response"
+  old_regenerated boolean not null default false,
+
+  -- Name of LLM used to generate this (or, for a user message, a reply to this)
+  generation_model text default null,
+
+  -- Number of tokens using the tokenization scheme of .generation_model
+  nb_tokens int default null
+);
+
+create unique index chat_msg_session_and_id on chat_message (session, id);
+
+-- Store embeddings for user messages only, for future analytics etc.
+create table chat_message_embedding (
+  msg ulid not null references chat_message (id) on delete cascade,
+
+  -- Name of the embedding model
+  model text not null,
+
+  -- Array of floats
+  embedding jsonb not null,
+
+  -- Number of tokens using the tokenization scheme of .model
+  nb_tokens int not null,
+
+  primary key (msg, model)
+);
+
+create table stored_dialogue (
+  id ulid not null default gen_ulid() primary key
+);
+
+create table dialogue_item (
+  id ulid not null default gen_ulid() primary key,
+  dialogue ulid not null references stored_dialogue (id) on delete cascade,
+
+  -- Represents a root if null
+  parent ulid default null references dialogue_item (id) on delete set null,
+
+  -- References dialogue_phrasing. Determine which phrasing is shown in the editing UI, does not affect generation.
+  canonical_phrasing ulid default null,
+
+  response text not null,
+
+  -- If true, user input will not match against this unless previously matched
+  -- this stored_dialogue. Useful for items like "yes" or "tell me more".
+  ignore_for_first_match boolean not null default false
+);
+
+-- User input is matched against all phrasings for an item, and the score for
+-- the item is the max score across all phrasings.
+create table dialogue_phrasing (
+  id ulid not null default gen_ulid() primary key,
+  dialogue_item ulid not null references dialogue_item (id) on delete cascade,
+  q_text text not null,
+
+  -- A matched "counterexample" phrasing will prevent this item from being
+  -- matched unless another phrasing is matched first, with a higher score.
+  is_counterexample boolean not null default false
+);
+
+-- alter table dialogue_item foreign key (canonical_phrasing) references dialogue_phrasing (id) on delete set null;
+-- The above gives ERROR:  XX000: could not find commutator for operator 16397
+
+create table dialogue_phrasing_embedding (
+  phrasing ulid not null references dialogue_phrasing (id) on delete cascade,
+
+  -- Name of the embedding model
+  model text not null,
+
+  -- Array of floats
+  embedding jsonb not null,
+
+  -- Number of tokens using the tokenization scheme of .model
+  nb_tokens int not null,
+
+  generation_time timestamptz not null default now(),
+
+  primary key (phrasing, model)
+);
+
+create table chat_reply_edit_log (
+  id ulid not null default gen_ulid() primary key,
+  reply_msg ulid not null references chat_message (id) on delete cascade,
+
+  -- id of the dialogue item added or updated
+  edited_dialogue ulid not null references dialogue_item (id) on delete cascade
+);
+
+-- Metadata for generated reply messages for future analytics.
+create table chat_reply_metadata (
+  reply_msg ulid not null references chat_message (id) on delete cascade primary key,
+
+  -- References to dialogue_phrasing
+  matched ulid[] not null,
+
+  match_scores float8[] not null,
+  best_match_dialogue ulid default null references dialogue_item (id) on delete set null,
+
+  -- The above 3 fields can be empty / null if no good match
+
+  -- References to chat_message
+  model_inputs ulid[] not null,
+
+  -- True if we bypassed the LLM due to high confidence
+  direct_result boolean not null default false,
+
+  -- enum: -1 = dislike, 0 = default, 1 = like
+  user_feedback int not null default 0,
+
+  regen_of ulid default null references chat_message (id) on delete set null,
+  last_edit ulid default null references chat_reply_edit_log (id) on delete set null
+);
+
+create table chat_suggestion (
+  reply_msg ulid not null references chat_message (id) on delete cascade,
+  suggestion text not null,
+
+  -- enum: 0 = none, 1 = clicked
+  user_action int not null default 0,
+
+  primary key (reply_msg, suggestion)
 );
