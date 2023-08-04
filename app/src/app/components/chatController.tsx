79ab1fbb366e37878304e057b6bd5c265c05ca9c
Mao <m@maowtm.org>
2023-08-04 23:50:28 +0100

    Handle SSE

--- app/src/app/components/chatController.tsx
+++ app/src/app/components/chatController.tsx
@@ -1,6 +1,6 @@
 import React, { RefObject } from "react";
 import { getCredentialManager, subscribe as credentialsSubscribe } from "app/utils/credentials";
-import { DefaultService as API, ChatSession, Message } from "app/openapi"
+import { DefaultService as API, ChatSession, ChatSuggestions, Message } from "app/openapi"
 import ChatSkeleton from "app/components/chatSkeleton";
 import StartNewChatButton from "./startNewChatButton";
 import * as classes from "./chatController.module.css"
@@ -35,6 +35,7 @@ interface S {
   messages_error: Error | null;
   no_permission: boolean;
   scrolling_to_bottom: boolean;
+  suggestions: ChatSuggestions | null;
 }
 
 interface P {
@@ -47,11 +48,15 @@ const InitialState: S = {
   messages_error: null,
   no_permission: false,
   scrolling_to_bottom: true,
+  suggestions: null,
 };
 
 export class ChatController extends React.Component<P, S> {
   containerRef: RefObject<HTMLDivElement>;
   unsubscribeCredentials: () => void;
+
+  sseEventSource: EventSource | null = null;
+  ssePingTimeout: number | null = null;
 
   constructor(props: P) {
     super(props);
@@ -87,6 +92,106 @@ export class ChatController extends React.Component<P, S> {
     return getCredentialManager().admin_token;
   }
 
+  updateMessage(message: Message) {
+    const messages = this.state.messages;
+
+    const insert_loc = messages.findIndex(m => m.id >= message.id);
+    if (insert_loc == -1) {
+      messages.push(message);
+    } else if (messages[insert_loc].id == message.id) {
+      messages[insert_loc] = message;
+    } else {
+      messages.splice(insert_loc, 0, message);
+    }
+
+    this.setState({
+      messages: messages,
+    });
+  }
+
+  stopSSE() {
+    if (this.ssePingTimeout) {
+      clearTimeout(this.ssePingTimeout);
+      this.ssePingTimeout = null;
+    }
+    if (this.sseEventSource) {
+      this.sseEventSource.close();
+      this.sseEventSource = null;
+    }
+  }
+
+  startSSE(): Promise<void> {
+    this.stopSSE();
+    let maybe_token = "";
+    if (this.chat_token) {
+      maybe_token = `chat_token=${encodeURIComponent(this.chat_token)}`;
+    }
+    let sseEventSource = new EventSource(`/api/v1/chat-session/${encodeURIComponent(this.props.chat_id)}/stream?${maybe_token}`);
+    this.sseEventSource = sseEventSource;
+    const ssePingTimeout = () => {
+      if (this.sseEventSource !== sseEventSource) {
+        return;
+      }
+      console.error("Did not receive SSE ping for 10 seconds, re-opening a new connection...");
+      this.startSSE().catch(err => {
+        console.error("Failed to re-open SSE connection:", err);
+        this.setState({ messages_error: new Error("Connection to server lost, latest messages may not be displayed.") });
+      });
+    }
+    const refreshSSEPingTimeout = () => {
+      if (this.sseEventSource !== sseEventSource) {
+        return;
+      }
+      if (this.ssePingTimeout) {
+        clearTimeout(this.ssePingTimeout);
+      }
+      this.ssePingTimeout = setTimeout(ssePingTimeout, 10000);
+    };
+    sseEventSource.addEventListener("message", evt => {
+      refreshSSEPingTimeout();
+      let msg: Message = JSON.parse(evt.data);
+      this.updateMessage(msg);
+    });
+    sseEventSource.addEventListener("deleteMessage", evt => {
+      let id = evt.data;
+      let idx = this.state.messages.findIndex(m => m.id == id);
+      if (idx != -1) {
+        this.state.messages.splice(idx, 1);
+        this.setState({ messages: this.state.messages });
+      }
+    });
+    sseEventSource.addEventListener("suggestions", evt => {
+      let suggestions: ChatSuggestions = JSON.parse(evt.data);
+      this.setState({ suggestions: suggestions });
+    });
+    sseEventSource.addEventListener("ping", evt => {
+      refreshSSEPingTimeout();
+    });
+    return new Promise((resolve, reject) => {
+      let resolved = false;
+      sseEventSource.addEventListener("open", evt => {
+        console.log("!")
+        resolve();
+        resolved = true;
+        refreshSSEPingTimeout();
+      });
+      sseEventSource.addEventListener("error", evt => {
+        if (resolved) {
+          console.error("SEE connection closed unexpectedly.");
+          this.startSSE().catch(e => {
+            console.error("Unable to restart SSE connection.", e);
+            this.setState({
+              messages_error: new Error("Connection to server lost, latest messages may not be displayed.")
+            });
+          });
+          return;
+        }
+        reject(new Error("Unable to start server-sent events connection."));
+        this.stopSSE();
+      });
+    });
+  }
+
   async initialize() {
     try {
       if (!this.chat_token && !this.admin_token) {
@@ -96,21 +201,33 @@ export class ChatController extends React.Component<P, S> {
         });
         return;
       }
-      let init_msg_list = await fetchChatData(this.props.chat_id);
-      this.setState({
-        messages: init_msg_list.messages,
-        initial_loading: false,
-        messages_error: null,
-      });
+      let arr = await Promise.allSettled([
+        this.startSSE(),
+        fetchChatData(this.props.chat_id).then(init_msg_list => {
+          this.setState({
+            messages: init_msg_list.messages,
+            suggestions: init_msg_list.last_suggestions,
+            initial_loading: false,
+            messages_error: null,
+          });
+        })
+      ]);
+      for (let elem of arr) {
+        if (elem.status == "rejected") {
+          throw elem.reason;
+        }
+      }
     } catch (e) {
       this.setState({
         messages_error: e,
         initial_loading: false,
       });
+      this.stopSSE();
     }
   }
 
   unload() {
+    this.stopSSE();
     this.setState(InitialState);
   }
 
@@ -128,15 +245,6 @@ export class ChatController extends React.Component<P, S> {
             this.state.no_permission)
             ? ` ${classes.messageListContainerCenter}` : ` ${classes.messageListContainerJustifyBottom}`)
         }>
-          {this.state.messages_error ? (
-            <ChatListError err={this.state.messages_error} onRetry={this.retryInitialLoad} />
-          ) : null}
-          {this.state.no_permission ? (
-            <div className={classes.noPermission}>
-              You do not have access to this chat.<br /><br />
-              <StartNewChatButton />
-            </div>
-          ) : null}
           {this.state.messages.length > 0 ? (
             <AutoScrollComponent containerRef={this.containerRef}>
               <ChatMessagesList messages_list={this.state.messages} enable_buttons={true} />
@@ -149,6 +257,15 @@ export class ChatController extends React.Component<P, S> {
               </>
             ) : null
           )}
+          {this.state.messages_error ? (
+            <ChatListError err={this.state.messages_error} onRetry={this.retryInitialLoad} />
+          ) : null}
+          {this.state.no_permission ? (
+            <div className={classes.noPermission}>
+              You do not have access to this chat.<br /><br />
+              <StartNewChatButton />
+            </div>
+          ) : null}
         </div>
         <MessageInputBox chat_id={this.props.chat_id} />
       </div>
