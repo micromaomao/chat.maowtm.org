ec58ec4f43f5c65f54969521c8d3549f6e978b5b
Mao <m@maowtm.org>
2023-08-19 22:37:34 +0100

    Fix migration

--- app/src/db/migrations/4.sql
+++ app/src/db/migrations/4.sql
@@ -4,6 +4,6 @@ alter table chat_reply_metadata alter column matched_dialogue_items type ulid[] 
 alter table chat_reply_metadata alter column model_chat_inputs type ulid[] using model_chat_inputs::ulid[];
 alter table chat_reply_metadata add column best_phrasing ulid[] not null default '{}';
 
-alter table chat_reply_metadata alter column matched_phrasings to __deleted_matched_phrasings;
-alter table chat_reply_metadata alter column match_scores to __deleted_match_scores;
-alter table chat_reply_metadata alter column best_match_dialogue to __deleted_best_match_dialogue;
+alter table chat_reply_metadata rename column matched_phrasings to __deleted_matched_phrasings;
+alter table chat_reply_metadata rename column match_scores to __deleted_match_scores;
+alter table chat_reply_metadata rename column best_match_dialogue to __deleted_best_match_dialogue;
