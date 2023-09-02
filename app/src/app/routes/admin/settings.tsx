2640b980740f21df7bb3ccb066ef4bba350949e2
Mao <m@maowtm.org>
2023-09-02 16:13:48 +0100

    Setting page enhancement

--- app/src/app/routes/admin/settings.tsx
+++ app/src/app/routes/admin/settings.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useMemo, useState } from "react";
-import { Button, Field, SelectTabData, Skeleton, SkeletonItem, Tab, TabList, Textarea, TextareaOnChangeData, Title2, Toast, ToastBody, ToastTitle, useToastController } from "@fluentui/react-components";
+import { Button, Dialog, DialogActions, DialogBody, DialogContent, DialogOpenChangeData, DialogSurface, DialogTitle, Field, SelectTabData, Skeleton, SkeletonItem, Tab, TabList, Textarea, TextareaOnChangeData, Title2, Toast, ToastBody, ToastTitle, useToastController } from "@fluentui/react-components";
 
 import * as classes from "./settings.module.css";
 import { useSharedState } from "app/utils/sharedstate";
@@ -72,11 +72,8 @@ function Component() {
     handleConfigRefresh();
   }, []);
 
-  function handleSelectTab(evt, data: SelectTabData) {
-    if (currentTab == data.value) {
-      return;
-    }
-    setCurrentTab(data.value as string);
+  function doSelectTab(tab: string) {
+    setCurrentTab(tab);
     setEdited(false);
   }
 
@@ -94,10 +91,13 @@ function Component() {
       <configActions.Provider value={{ refresh: handleConfigRefresh, save: handleSave, markEdited: handleMarkEdited }}>
         <div className={classes.container}>
           <Title2 as="span">Settings</Title2>
-          <TabList selectedValue={currentTab} onTabSelect={handleSelectTab}>
-            <Tab value="prompt" icon={<TextboxRegular />}>Prompt{edited && currentTab == "prompt" ? "*" : null}</Tab>
-            <Tab value="rawConfig" icon={<BracesFilled />}>Edit JSON config{edited && currentTab == "rawConfig" ? "*" : null}</Tab>
-          </TabList>
+          <ConfigTabList
+            {...{ currentTab, edited, doSelectTab }}
+            tabs={{
+              prompt: { icon: <TextboxRegular />, label: "Prompt" },
+              rawConfig: { icon: <BracesFilled />, label: "Edit JSON config" },
+            }}
+          />
 
           {configCtx.loading ? (
             <Skeleton style={{ padding: "20px" }}>
@@ -116,6 +116,61 @@ function Component() {
         </div>
       </configActions.Provider>
     </configContext.Provider>
+  )
+}
+
+interface ConfigTabListProps {
+  currentTab: string;
+  doSelectTab(key: string): void;
+  edited: boolean;
+  tabs: Record<string, { icon: React.ReactElement, label: string }>;
+}
+
+function ConfigTabList({ currentTab, doSelectTab, edited, tabs }: ConfigTabListProps) {
+  const [switchTabConfirmDialogTarget, setSwitchTabConfirmDialogTarget] = useState<string | null>(null);
+
+  function handleSelectTab(evt, data: SelectTabData) {
+    let tab = data.value as string;
+    if (tab == currentTab) {
+      return;
+    }
+    if (!edited) {
+      doSelectTab(tab);
+    } else {
+      setSwitchTabConfirmDialogTarget(tab);
+    }
+  }
+
+  function handleDialogOpenChange(evt, data: DialogOpenChangeData) {
+    if (!data.open) {
+      setSwitchTabConfirmDialogTarget(null);
+    }
+  }
+
+  return (
+    <>
+      <TabList selectedValue={currentTab} onTabSelect={handleSelectTab}>
+        {Object.entries(tabs).map(([key, val]) => (
+          <Tab key={key} value={key} icon={val.icon}>{val.label}{edited && currentTab == key ? "*" : ""}</Tab>
+        ))}
+      </TabList>
+      <Dialog open={switchTabConfirmDialogTarget !== null} modalType="modal" onOpenChange={handleDialogOpenChange}>
+        <DialogSurface>
+          <DialogBody>
+            <DialogContent>
+              Switching tabs will discard your unsaved changes. Are you sure you want to continue?
+            </DialogContent>
+            <DialogActions>
+              <Button appearance="secondary" onClick={() => setSwitchTabConfirmDialogTarget(null)}>Cancel</Button>
+              <Button appearance="primary" onClick={() => {
+                setSwitchTabConfirmDialogTarget(null);
+                doSelectTab(switchTabConfirmDialogTarget);
+              }}>Discard</Button>
+            </DialogActions>
+          </DialogBody>
+        </DialogSurface>
+      </Dialog>
+    </>
   )
 }
 
