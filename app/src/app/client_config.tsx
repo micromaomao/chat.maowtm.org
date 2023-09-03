657b73b24b2ce7070cafa740627148dcd1bf24e4
Mao <m@maowtm.org>
2023-09-03 01:48:52 +0100

    Yuki customization

--- app/src/app/client_config.tsx
+++ app/src/app/client_config.tsx
@@ -5,10 +5,10 @@ import { EditRegular } from "@fluentui/react-icons";
 const API_BASE = '/api/v1';
 
 // Consider also changing the <title> tag in app/src/index.html
-const PROJECT_NAME = "Yet another chatbot";
+const PROJECT_NAME = "Yuki Nagato";
 
 // If you change the URL here, consider also changing the <meta rel="icon"> tag in index.html.
-import iconUrl from "../../assets/icon.svg"
+import iconUrl from "../../assets/icon.png"
 
 const css: Record<string, CSSProperties> = {
   box: {
@@ -38,7 +38,7 @@ const css: Record<string, CSSProperties> = {
 
 export function HomePageHeader() {
   return (<>
-    <Image src={iconUrl} style={{ width: "100px", height: "100px" }} fit="contain" />
+    <Image src={iconUrl} style={{ width: "100px", height: "100px", borderRadius: "50%" }} fit="contain" />
     <Title1 align="center">{PROJECT_NAME}</Title1>
   </>);
 }
@@ -49,32 +49,9 @@ export function HomePageHeader() {
 export function HomePageFooter() {
   return (<>
     <div style={{ height: "20px", flexShrink: "1", flexGrow: "0" }} />
-    <Title2 align="center">Getting started&hellip;</Title2>
     <div style={css.box}>
-      <EditRegular style={css.icon} />
-      <Body2 style={css.text}>
-        To start building your chatbot, click "Admin Login" below and follow the
-        instruction to log in as admin to change the prompt and other settings,
-        and start chatting! <br style={{ marginBottom: "10px" }} />
-
-        As you chat with the chatbot, you can provide additional ad-hoc stored
-        sample responses by clicking on the <EditRegular style={{
-          transform:
-            "translateY(2px)"
-        }} /> icon next to a message. Try experimenting with different ways of
-        phrasing input text and responses to get the best result. Stored
-        responses are organized in a tree, and this will influence how the
-        sample is matched with the user input. <br style={{ marginBottom: "10px" }} />
-
-        Stored responses, as well as all other settings, are only visible and
-        editable by admin.
-      </Body2>
+      Entertainment purpose only. Currently powered by OpenAI.
     </div>
-    <Body2 style={css.text}>
-      Most backend settings are changable in the admin interface. However, to
-      change this page, you must fork the project and change
-      client_config.tsx, then build your own version of the container.
-    </Body2>
   </>);
 }
 
@@ -83,22 +60,22 @@ export function HomePageFooter() {
 // You just need the BrandVariants bit.
 
 const maochatTheme: BrandVariants = {
-  10: "#020402",
-  20: "#101C15",
-  30: "#162E21",
-  40: "#1A3C29",
-  50: "#1D4A32",
-  60: "#1F583B",
-  70: "#226745",
-  80: "#24774E",
-  90: "#258658",
-  100: "#269662",
-  110: "#33A66E",
-  120: "#57B281",
-  130: "#75BF95",
-  140: "#90CCA8",
-  150: "#ABD8BD",
-  160: "#C6E4D1"
+  10: "#0A0006",
+  20: "#21131B",
+  30: "#321F2B",
+  40: "#402A38",
+  50: "#4E3645",
+  60: "#5C4252",
+  70: "#6A4E5F",
+  80: "#795B6D",
+  90: "#87687B",
+  100: "#967689",
+  110: "#A58498",
+  120: "#B492A6",
+  130: "#C3A0B5",
+  140: "#D2AFC4",
+  150: "#E1BED3",
+  160: "#EFCEE2"
 };
 
 export { API_BASE, maochatTheme, PROJECT_NAME };
